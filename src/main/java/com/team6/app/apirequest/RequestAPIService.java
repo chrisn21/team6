566a6945a8b57cbf5fecc9eb4fa714e4c872a425
chrisn21 <chrisnguyen021@gmail.com>
2014-02-14 13:40:09 -0800

    revised requestapi function

--- src/main/java/com/team6/app/apirequest/RequestAPIService.java
+++ src/main/java/com/team6/app/apirequest/RequestAPIService.java
@@ -1,7 +1,5 @@
 package com.team6.app.apirequest;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -9,9 +7,10 @@ import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.mashape.unirest.http.JsonNode;
-import com.team6.app.quiz.Question;
+import com.team6.app.Constants;
  
  
 @Repository
@@ -20,18 +19,27 @@ public class RequestAPIService {
     @Autowired
     private MongoTemplate mongoTemplate;
      
-   public List<Question> parseQuestions(JsonNode j) throws JSONException
+   public ModelAndView parseQuestions(JsonNode j) throws JSONException
    {
+	   ModelAndView mv = new ModelAndView(Constants.QUIZ_CREATE_PATH_FILE);
 	   JSONObject obj = new JSONObject(j.toString());
 	   JSONArray results = obj.getJSONArray("result");
-	   List<Question> lq = new ArrayList<Question>();
+	   String[] questions = new String[results.length()];
+	   String[] answers = new String[results.length()];
+	   String category = "";
 	   for(int i = 0; i < results.length(); i++)
 	   {
 		   JSONObject question = results.getJSONObject(i);
-		   Question q = new Question(question.getString("category"), question.getString("answer"), question.getInt("difficulty"), question.getString("question"));
-		   lq.add(q);
+		   questions[i] = question.getString("question");
+		   answers[i] = question.getString("answer");
+		   category = question.getString("category");
+		   //Question q = new Question(question.getString("category"), question.getString("answer"), question.getInt("difficulty"), question.getString("question"));
 	   }
-	   return lq;
+	   
+	   mv.addObject("name", category);
+	   mv.addObject("questions[]", questions);
+	   mv.addObject("answers[]", answers);
+	   return mv;
    }
 
     
